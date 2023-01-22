525564f084a75847b2384c8b8b3b1d759af62ede
kayaba-akihiko <kayaba.fluctlight@gmail.com>
2023-01-22 22:24:44 +0700

    smaller waitForSocket interval, faster load time

--- game/assets/scripts/network/index.ts
+++ game/assets/scripts/network/index.ts
@@ -28,7 +28,7 @@ connectionInstance.onopen = () => {
 	system.isSocketReady = true;
 };
 
-export const waitForSocket = (maxRetry = 100): Promise<boolean> => {
+export const waitForSocket = (maxRetry = 150): Promise<boolean> => {
 	let retryCount = 0;
 
 	return new Promise((resolve, reject) => {
@@ -37,12 +37,12 @@ export const waitForSocket = (maxRetry = 100): Promise<boolean> => {
 				resolve(true);
 				clearInterval(waitInterval);
 			} else if (retryCount > maxRetry) {
-				reject('too many retries');
+				reject('too many retries, socket may not be ready');
 				clearInterval(waitInterval);
 			}
 
 			retryCount += 1;
-		}, 500);
+		}, 200);
 	});
 };
 
